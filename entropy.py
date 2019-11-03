35111586b39e6fc12ae64c70ec9565eb4f7373d0
Tomas Karella <tomas.karella@gmail.com>
2019-11-03 21:47:31 +0100

    Shapes added to the outputs

--- entropy.py
+++ entropy.py
@@ -3,6 +3,7 @@ from preprocess.reader import read_nii_arrays
 from preprocess.analyzer import voxel_dist_operations
 import pickle
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Characteristics of images.")
 parser.add_argument("--data", "-d", 
@@ -24,7 +25,7 @@ args = parser.parse_args()
 
 data  = read_nii_arrays(args.data, image_ext=args.ext, default_shape=(256, 256, 166))
 norm_data = normalize(data)
-result = voxel_dist_operations(norm_data)
-with open(args.output, "wb") as f: 
+result = voxel_dist_operations(norm_data, [np.max, np.mean, np.var, np.min, np.median])
+result["shapes"] = norm_data.shape
+with open(args.output, "wb") as f:
     pickle.dump(result, f, protocol=pickle.HIGHEST_PROTOCOL)
-    
