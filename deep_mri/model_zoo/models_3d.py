1b9ecd86a9aab8fbcbd4ca068db03be3dc32821b
Tomas Karella <tomas.karella@profinit.eu>
2020-06-11 10:59:28 +0200

    Pretrained conv

--- deep_mri/model_zoo/models_3d.py
+++ deep_mri/model_zoo/models_3d.py
@@ -1,24 +1,45 @@
 import tensorflow as tf
 
+
 def payan_montana_model(input_shape=(97, 115, 97, 1),
                         conv_filters_count=150,
                         fc_size=800):
-    #TODO: add paper name
+    # TODO: add paper name
     assert len(input_shape) == 4
 
     input_layer = tf.keras.layers.Input(input_shape, name=f'Input')
-    conv_layer1 = tf.keras.layers.Convolution3D(conv_filters_count, (5,5,5), name='Conv-1', activation='relu')(input_layer)
-    conv_layer2 = tf.keras.layers.Convolution3D(conv_filters_count, (5,5,5), name='Conv-2', activation='relu')(input_layer)
-    conv_layer3 = tf.keras.layers.Convolution3D(conv_filters_count, (5,5,5), name='Conv-3', activation='relu')(input_layer)
-    maxp_layer1 = tf.keras.layers.MaxPool3D(name='MaxP-1', pool_size=(5,5,5))(conv_layer1)
-    maxp_layer2 = tf.keras.layers.MaxPool3D(name='MaxP-2', pool_size=(5,5,5))(conv_layer2)
-    maxp_layer3 = tf.keras.layers.MaxPool3D(name='MaxP-3', pool_size=(5,5,5))(conv_layer3)
+    conv_layer1 = tf.keras.layers.Convolution3D(conv_filters_count, (5, 5, 5), name='Conv-1', activation='relu')(
+        input_layer)
+    conv_layer2 = tf.keras.layers.Convolution3D(conv_filters_count, (5, 5, 5), name='Conv-2', activation='relu')(
+        input_layer)
+    conv_layer3 = tf.keras.layers.Convolution3D(conv_filters_count, (5, 5, 5), name='Conv-3', activation='relu')(
+        input_layer)
+    maxp_layer1 = tf.keras.layers.MaxPool3D(name='MaxP-1', pool_size=(5, 5, 5))(conv_layer1)
+    maxp_layer2 = tf.keras.layers.MaxPool3D(name='MaxP-2', pool_size=(5, 5, 5))(conv_layer2)
+    maxp_layer3 = tf.keras.layers.MaxPool3D(name='MaxP-3', pool_size=(5, 5, 5))(conv_layer3)
     maxp_layer1 = tf.keras.layers.Flatten()(maxp_layer1)
     maxp_layer2 = tf.keras.layers.Flatten()(maxp_layer2)
     maxp_layer3 = tf.keras.layers.Flatten()(maxp_layer3)
     conv_layer = tf.keras.layers.Concatenate(name='Concat')([maxp_layer1, maxp_layer2, maxp_layer3])
     flat_layer = tf.keras.layers.Flatten()(conv_layer)
-    fc_layer = tf.keras.layers.Dense(fc_size,name=f'FC-{fc_size}', activation='relu')(flat_layer)
+    fc_layer = tf.keras.layers.Dense(fc_size, name=f'FC-{fc_size}', activation='relu')(flat_layer)
     output_layer = tf.keras.layers.Dense(3, name='Classification', activation='softmax')(fc_layer)
 
     return tf.keras.Model(input_layer, output_layer)
+
+
+def payan_montana_model_pretrained_conv(path_to_model, input_shape=(97, 115, 97, 1), fc_size=800):
+    pretrained_model = tf.keras.models.load_model(path_to_model)
+    trained_layer = pretrained_model.get_layer('Encoder-Conv')
+    big_model = payan_montana_model(input_shape=input_shape,
+                                    fc_size=fc_size,
+                                    conv_filters_count=trained_layer.get_config()['filters'])
+    pretrained_weights = trained_layer.get_weights()
+    big_model.get_layer('Conv-1').trainable = False
+    big_model.get_layer('Conv-2').trainable = False
+    big_model.get_layer('Conv-3').trainable = False
+    big_model.get_layer('Conv-1').set_weights(pretrained_weights)
+    big_model.get_layer('Conv-2').set_weights(pretrained_weights)
+    big_model.get_layer('Conv-3').set_weights(pretrained_weights)
+
+    return big_model
