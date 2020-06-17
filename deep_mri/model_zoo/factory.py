6696e53f765403e120d3f748163070e882ccc2e0
Tomas Karella <tomas.karella@profinit.eu>
2020-06-17 19:55:48 +0200

    Fixing bug

--- deep_mri/model_zoo/factory.py
+++ deep_mri/model_zoo/factory.py
@@ -1,4 +1,4 @@
-from deep_mri.model_zoo import models_3d, encoders
+from deep_mri.model_zoo import models_3d, encoders, models_2d
 
 MODEL_TYPE_DELIMETER = "_"
 
@@ -7,10 +7,10 @@ def model_factory(model_name, **model_args):
     assert len(model_name.split(MODEL_TYPE_DELIMETER)) == 2, "Model name can have only single delimeter"
     mod_type, mod_name = model_name.split(MODEL_TYPE_DELIMETER)
     if mod_type.lower() == "3d":
-        return models_3d.factory(model_name, **model_args)
+        return models_3d.factory(mod_name, **model_args)
     elif mod_type.lower() == "encoder":
-        return encoders.factory(model_name, **model_args)
+        return encoders.factory(mod_name, **model_args)
     elif mod_type.lower() == "2d":
-        return models_2d.factory(model_name, **model_args)
+        return models_2d.factory(mod_name, **model_args)
     else:
         raise Exception(f"Unknown model name: {model_name}")
