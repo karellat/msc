fa26f10a1348a551ba436383c35bd7a878b57538
Tomas Karella <tomas.karella@profinit.eu>
2020-06-17 21:50:32 +0200

    Bugfix

--- run_train.py
+++ run_train.py
@@ -4,9 +4,9 @@ from deep_mri.train.training import run_train
 
 @click.command()
 @click.option("--config_file", type=click.Path(exists=True))
-def run(path_to_config):
+def main(config_file):
     run_train(path_to_config)
 
 
 if __name__ == "__main__":
-    run_train()
+    main()
