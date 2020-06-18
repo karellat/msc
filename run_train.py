3658684ec0f3cdd9190afa918cb78088bdc9ac7d
Tomas Karella <tomas.karella@profinit.eu>
2020-06-18 15:09:26 +0200

    Logging info

--- run_train.py
+++ run_train.py
@@ -1,10 +1,12 @@
 import click
+import logging
 from deep_mri.train.training import run_train
 
 
 @click.command()
 @click.option("-c", "--config_file", type=click.Path(exists=True))
 def main(config_file):
+    logging.basicConfig(level=logging.INFO)
     run_train(config_file)
 
 
