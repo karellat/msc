da6843ae3e45439a8e8e260c912d5eaf18d9f66d
Tomas Karella <tomas.karella@gmail.com>
2020-06-28 17:54:11 +0200

    Transfer model

--- deep_mri/model_zoo/models_2d.py
+++ deep_mri/model_zoo/models_2d.py
@@ -1,2 +1,24 @@
+import tensorflow_hub as hub
+import tensorflow as tf
+
+
+def transfer_model(feature_extractor_url,
+                   input_shape=(193, 193, 3),
+                   feature_trainable=False,
+                   fc_count=1024):
+    feature_extractor_layer = hub.KerasLayer(feature_extractor_url,
+                                             input_shape=input_shape,
+                                             trainable=feature_trainable)
+    return tf.keras.Sequential([
+        feature_extractor_layer,
+        tf.keras.layers.Dense(fc_count, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(3, activation='softmax')
+    ])
+
+
 def factory(model_name, **model_args):
-    raise NotImplementedError("None 2D model implemented")
+    if model_name.lower() == "transfer":
+        transfer_model(**model_args)
+    else:
+        raise Exception(f"Unknown 3d model : {model_name}")
