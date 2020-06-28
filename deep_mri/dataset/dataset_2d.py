05f906db40e0d4ddb4d656ac4293de2eca5b839b
Tomas Karella <tomas.karella@gmail.com>
2020-06-28 17:13:05 +0200

    2d dataset changed

--- deep_mri/dataset/dataset_2d.py
+++ deep_mri/dataset/dataset_2d.py
@@ -1,95 +1,49 @@
-import logging
-import tensorflow as tf
 import os
-import math
-from enum import Enum, auto
-from deep_mri.dataset import CLASS_NAMES
-
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-DATASET_SIZE = 21140
+import random
+import numpy as np
+import tensorflow as tf
+from deep_mri.dataset.dataset import CLASS_NAMES
 
 
-def _get_label(file_path, class_folder=3):
+def _get_label_tf(file_path, class_folder=-4):
     parts = tf.strings.split(file_path, os.path.sep)
     return parts[class_folder] == CLASS_NAMES
 
 
-class ImgReshape(Enum):
-    RESIZE = auto()
-    RESIZE_CROP_PAD = auto()
-    RESIZE_PAD = auto()
-
-
-def _decode_img(img,
-                out_shape=(193, 193),
-                reshape_method=ImgReshape.RESIZE):
-    img = tf.image.decode_png(img, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    if out_shape is None:
-        return img
-    else:
-        if reshape_method == ImgReshape.RESIZE:
-            return tf.image.resize(img, out_shape)
-        elif reshape_method == ImgReshape.RESIZE_CROP_PAD:
-            return tf.image.resize_with_crop_or_pad(img, target_height=out_shape[0], target_width=out_shape[1])
-        elif reshape_method == ImgReshape.RESIZE_PAD:
-            return tf.image.resize_with_pad(img, target_height=out_shape[0], target_width=out_shape[1])
-
-
-def _process_path(file_path, out_shape=(193, 193), reshape_method=ImgReshape.RESIZE):
-    label = _get_label(file_path)
+def _process_path(file_path, img_size):
     img = tf.io.read_file(file_path)
-    img = _decode_img(img,
-                      out_shape=out_shape,
-                      reshape_method=reshape_method)
-
+    img = tf.io.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.resize_with_crop_or_pad(img,
+                                           target_height=img_size,
+                                           target_width=img_size)
+    label = _get_label_tf(file_path)
     return img, label
 
 
-def _prepare_for_training(ds,
-                          dataset_size,
-                          shuffle=True,
-                          cache=True,
-                          shuffle_buffer_size=1000):
-    # This is a small dataset, only load it once, and keep it in memory.
-    # use `.cache(filename)` to cache preprocessing work for datasets that don't
-    # fit in memory.
-    if cache:
-        if isinstance(cache, str):
-            ds = ds.cache(cache)
-        else:
-            ds = ds.cache()
-
-        valid_size = int(math.floor(dataset_size / 10.0))
-        train_ds = ds.skip(valid_size * 2)
-        valid_ds = ds.take(valid_size * 2).skip(valid_size)
-        test_ds = ds.take(valid_size)
-
-        # SHUFFLE by images
-        if shuffle:
-            train_ds = train_ds.shuffle(buffer_size=shuffle_buffer_size)
-            valid_ds = valid_ds.shuffle(buffer_size=shuffle_buffer_size)
-            test_ds = test_ds.shuffle(buffer_size=shuffle_buffer_size)
-
-        # `prefetch` lets the dataset fetch batches in the background while the model
-        # is training.
-        train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
-        valid_ds = valid_ds.prefetch(buffer_size=AUTOTUNE)
-        test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
-
-        return train_ds, valid_ds, test_ds
+def _generator(file_list, img_size):
+    for file_name in file_list:
+        img, label = _process_path(file_name, img_size)
+        yield img, label
 
 
-def factory(files_list,
-            dataset_size=DATASET_SIZE,
-            out_shape=(193, 193),
-            reshape_method=ImgReshape.RESIZE,
-            shuffle=True):
-    labeled_ds = files_list.map(
-        lambda file_path: _process_path(file_path,
-                                        out_shape=out_shape,
-                                        reshape_method=reshape_method),
-        num_parallel_calls=AUTOTUNE)
+def factory(train_files, valid_files, img_size=193, shuffle=True, seed=42):
+    rnd = random.Random(seed)
+    img_shape = np.array((img_size, img_size, 3)).astype(int)
+    if shuffle:
+        rnd.shuffle(train_files)
+        rnd.shuffle(valid_files)
 
-    return _prepare_for_training(labeled_ds, dataset_size=dataset_size, shuffle=shuffle)
+    train_ds = tf.data.Dataset.from_generator(_generator,
+                                              output_types=(tf.float32, tf.bool),
+                                              output_shapes=(img_shape, (3,)),
+                                              args=[train_files, img_size])
+    valid_ds = tf.data.Dataset.from_generator(_generator,
+                                              output_types=(tf.float32, tf.bool),
+                                              output_shapes=(img_shape, (3,)),
+                                              args=[valid_files, img_size])
+
+    train_ds = train_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    valid_ds = valid_ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    return train_ds, valid_ds
--- deep_mri/dataset/dataset_factory.py
+++ deep_mri/dataset/dataset_factory.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from deep_mri.dataset import DEFAULT_PATH, DEFAULT_2D_PATH, DEFAULT_CSV_PATH
 from deep_mri.dataset import dataset_3d, dataset_2d, dataset_encoder
-from deep_mri.dataset.dataset import get_train_valid_files
+from deep_mri.dataset.dataset import get_train_valid_files, _get_image_group
 
 
 def dataset_factory(dataset_name,
@@ -19,11 +19,13 @@ def dataset_factory(dataset_name,
                                                          valid_filter_first_screen=valid_filter_first_screen)
         return dataset_3d.factory(train_files, valid_files, **dataset_args)
     elif dataset_name.lower() == "2d":
-        if train_filter_first_screen or valid_filter_first_screen:
-            raise NotImplementedError()
         data_path = DEFAULT_2D_PATH if data_path is None or data_path == 'default' else data_path
-        files_list = tf.data.Dataset.list_files(data_path)
-        return dataset_2d.factory(files_list, **dataset_args)
+        train_files, valid_files = get_train_valid_files(path=data_path,
+                                                         csv_path=data_csv_path,
+                                                         train_filter_first_screen=train_filter_first_screen,
+                                                         valid_filter_first_screen=valid_filter_first_screen,
+                                                         img_group_fnc=lambda x: _get_image_group(x, class_folder=-4))
+        return dataset_2d.factory(train_files, valid_files, **dataset_args)
     elif dataset_name.lower() == "encoder":
         data_path = DEFAULT_PATH if data_path is None or data_path == 'default' else data_path
         train_files, valid_files = get_train_valid_files(path=data_path,
