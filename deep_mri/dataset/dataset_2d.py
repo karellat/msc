306cb50a3f6959402263a032c214bbfe24fe28ba
Tomas Karella <tomas.karella@gmail.com>
2020-07-01 22:41:48 +0200

    Bugfix

--- deep_mri/dataset/dataset_2d.py
+++ deep_mri/dataset/dataset_2d.py
@@ -15,7 +15,7 @@ def _process_path(file_path, img_size, channels, class_names, transform):
     img = tf.io.read_file(file_path)
     img = tf.io.decode_image(img, channels=channels)
     if transform is not None:
-        img = transform(img)
+        img = _aug_factory(transform, img)
     img = tf.image.convert_image_dtype(img, tf.float32)
     img = tf.image.resize_with_crop_or_pad(img,
                                            target_height=img_size,
@@ -27,32 +27,32 @@ def _process_path(file_path, img_size, channels, class_names, transform):
 def _generator(file_list, img_size, channels, class_names, transform):
     for file_name in file_list:
         # Return both transformed and normal img
-        img, label = _process_path(file_name, img_size, channels, class_names)
+        img, label = _process_path(file_name, img_size, channels, class_names, transform=None)
         yield img, label
         if transform is not None:
             img, label = _process_path(file_name, img_size, channels, class_names, transform)
             yield img, label
 
 
-def _aug_factory(name):
+def _aug_factory(name, image):
     # TODO: Remove fixed values
     if name == 'saturation':
-        return lambda x: tf.image.adjust_saturation(x, 2)
+        return tf.image.adjust_saturation(image, 2)
     elif name == 'brightness':
-        return lambda x: tf.image.adjust_brightness(x, 0.1)
+        return tf.image.adjust_brightness(image, 0.1)
     elif name == 'blur':
-        return tfa.image.gaussian_filter2d
+        return tfa.image.gaussian_filter2d(image)
     elif name == 'mean':
-        return tfa.image.mean_filter2d
+        return tfa.image.mean_filter2d(image)
     elif name == 'median':
-        return tfa.image.median_filter2d
+        return tfa.image.median_filter2d(image)
     elif name == 'contrast_up':
-        return lambda x: tf.image.adjust_contrast(x, 1.2)
+        return tf.image.adjust_contrast(image, 1.2)
     elif name == 'contrast_down':
-        return lambda x: tf.image.adjust_contrast(x, 0.8)
+        return tf.image.adjust_contrast(image, 0.8)
     elif name == 'crop':
-        return lambda x: tf.resize(
-            tf.image.random_crop(x, size=tf.constant([96, 96, 3])),
+        return tf.resize(
+            tf.image.random_crop(image, size=tf.constant([96, 96, 3])),
             x.shape[:-1])
     else:
         raise Exception(f"Unknown data augmentation function {name}")
@@ -67,11 +67,10 @@ def factory(train_files, valid_files, dropping_group=None, img_size=193, channel
         rnd.shuffle(train_files)
         rnd.shuffle(valid_files)
 
-    transform_fnc = _aug_factory(transform)
     train_ds = tf.data.Dataset.from_generator(_generator,
                                               output_types=(tf.float32, tf.bool),
                                               output_shapes=(img_shape, (3,)),
-                                              args=[train_files, img_size, channels, class_names, transform_fnc])
+                                              args=[train_files, img_size, channels, class_names, transform])
     valid_ds = tf.data.Dataset.from_generator(_generator,
                                               output_types=(tf.float32, tf.bool),
                                               output_shapes=(img_shape, (3,)),
