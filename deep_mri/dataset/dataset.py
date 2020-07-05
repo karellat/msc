e15c639bd3575da75f4f4b38498ab6bab29497b7
Tomas Karella <tomas.karella@profinit.eu>
2020-07-05 18:36:12 +0200

    Shuffle by patient and random

--- deep_mri/dataset/dataset.py
+++ deep_mri/dataset/dataset.py
@@ -8,10 +8,12 @@ from nibabel import Nifti2Image
 from auto_tqdm import tqdm
 import pandas as pd
 import re
+from enum import Enum, auto
 
 from deep_mri.dataset import DEFAULT_PATH, CLASS_NAMES, DEFAULT_CSV_PATH
 
 DEFAULT_CLASS_FOLDER = -3
+
 
 def _merge_items(dictionary):
     items = []
@@ -52,79 +54,125 @@ def _get_image_group(file_path, class_folder=DEFAULT_CLASS_FOLDER):
     return parts[class_folder] == CLASS_NAMES
 
 
+class ShuffleStrategy(Enum):
+    SHUFFLE_RANDOM = auto()
+    SHUFFLE_SUBJECTS = auto()
+
+
 def get_train_valid_files(path=DEFAULT_PATH,
                           csv_path=DEFAULT_CSV_PATH,
                           train_filter_first_screen=True,
                           valid_filter_first_screen=False,
+                          shuffle_stategy=ShuffleStrategy.SHUFFLE_SUBJECTS,
                           valid_train_ratio=0.2,
                           shuffle=False,
                           dropping_group=None,
                           im_id_fnc=_get_image_id,
                           img_group_fnc=_get_image_group):
-
     assert dropping_group not in CLASS_NAMES, f"Uknown group to drop {dropping_group}"
+    assert isinstance(shuffle_stategy, ShuffleStrategy)
+
     files_list = glob.glob(path)
     # meta info
     df = pd.read_csv(csv_path)
     df = df.set_index('Image Data ID')
     df['Group'] = df['Group'].str.lower()
     meta_info = df[['Visit', 'Group', 'Subject']].to_dict('index')
+    if shuffle_stategy == ShuffleStrategy.SHUFFLE_SUBJECTS:
+        # Split into groups by subject id
+        subjects = {c: [] for c in CLASS_NAMES}
+        for f in files_list:
+            image_id = int(im_id_fnc(f))
+            target = CLASS_NAMES[np.argmax(img_group_fnc(f))]
+            assert target == meta_info[image_id]['Group']
+            subject = meta_info[image_id]['Subject']
+            visit = meta_info[image_id]['Visit']
+            if visit == 1:
+                subjects[target].append(subject)
 
-    # Split into groups by subject id
-    subjects = {c: [] for c in CLASS_NAMES}
-    for f in files_list:
-        image_id = int(im_id_fnc(f))
-        target = CLASS_NAMES[np.argmax(img_group_fnc(f))]
-        assert target == meta_info[image_id]['Group']
-        subject = meta_info[image_id]['Subject']
-        visit = meta_info[image_id]['Visit']
-        if visit == 1:
-            subjects[target].append(subject)
+        # Shuffle
+        rnd = random.Random(42)
+        if shuffle:
+            for group in subjects:
+                rnd.shuffle(group)
 
-    # Shuffle
-    rnd = random.Random(42)
-    if shuffle:
-        for group in subjects:
-            rnd.shuffle(group)
+        # Count groups
+        groups_count = np.array([len(subjects[key]) for key in subjects.keys()])
+        for count, group in zip(groups_count, subjects.keys()):
+            logging.warning(f'{group.upper()} count: {count}')
 
-    # Count groups
-    groups_count = np.array([len(subjects[key]) for key in subjects.keys()])
-    for count, group in zip(groups_count, subjects.keys()):
-        logging.warning(f'{group.upper()} count: {count}')
+        # Split Subjects into train valid groups
+        valid_sizes = np.ceil(groups_count * valid_train_ratio).astype(int)
+        train_subjects = {key: subjects[key][valid_size:] for key, valid_size in zip(subjects.keys(), valid_sizes)}
+        valid_subjects = {key: subjects[key][:valid_size] for key, valid_size in zip(subjects.keys(), valid_sizes)}
 
-    # Split Subjects into train valid groups
-    valid_sizes = np.ceil(groups_count * valid_train_ratio).astype(int)
-    train_subjects = {key: subjects[key][valid_size:] for key, valid_size in zip(subjects.keys(), valid_sizes)}
-    valid_subjects = {key: subjects[key][:valid_size] for key, valid_size in zip(subjects.keys(), valid_sizes)}
+        # Groups changed after visits
+        train_subjects = _merge_items(train_subjects)
+        valid_subjects = _merge_items(valid_subjects)
 
-    # Groups changed after visits
-    train_subjects = _merge_items(train_subjects)
-    valid_subjects = _merge_items(valid_subjects)
-
-    train_files = []
-    valid_files = []
-    for f in files_list:
-        image_id = int(im_id_fnc(f))
-        target = CLASS_NAMES[np.argmax(img_group_fnc(f))]
-        assert target == meta_info[image_id]['Group']
-        subject = meta_info[image_id]['Subject']
-        visit = meta_info[image_id]['Visit']
-        # Drop unwanted groups
-        if target == dropping_group:
-            continue
-        if subject in train_subjects:
-            if train_filter_first_screen and visit != 1:
+        train_files = []
+        valid_files = []
+        for f in files_list:
+            image_id = int(im_id_fnc(f))
+            target = CLASS_NAMES[np.argmax(img_group_fnc(f))]
+            assert target == meta_info[image_id]['Group']
+            subject = meta_info[image_id]['Subject']
+            visit = meta_info[image_id]['Visit']
+            # Drop unwanted groups
+            if target == dropping_group:
                 continue
-            train_files.append(f)
-        elif subject in valid_subjects:
-            if valid_filter_first_screen and visit != 1:
-                continue
-            valid_files.append(f)
-        else:
-            assert visit != 1, "None seen imgs"
-            logging.error(f"Image {image_id} without first visit, subject {subject}")
-            if not train_filter_first_screen:
-                logging.error(f"{image_id} appending to train set")
+            if subject in train_subjects:
+                if train_filter_first_screen and visit != 1:
+                    continue
                 train_files.append(f)
+            elif subject in valid_subjects:
+                if valid_filter_first_screen and visit != 1:
+                    continue
+                valid_files.append(f)
+            else:
+                assert visit != 1, "None seen imgs"
+                logging.error(f"Image {image_id} without first visit, subject {subject}")
+                if not train_filter_first_screen:
+                    logging.error(f"{image_id} appending to train set")
+                    train_files.append(f)
 
-    return train_files, valid_files
+        return train_files, valid_files
+    else:
+        # Split into groups by subject id
+        groups = {c: [] for c in CLASS_NAMES}
+        non_first_visit_files = []
+        for f in files_list:
+            image_id = int(im_id_fnc(f))
+            target = CLASS_NAMES[np.argmax(img_group_fnc(f))]
+            assert target == meta_info[image_id]['Group']
+            visit = meta_info[image_id]['Visit']
+            if (train_filter_first_screen or valid_filter_first_screen) and visit == 1:
+                non_first_visit_files.append(f)
+            else:
+                groups[target].append(f)
+        # Shuffle
+        rnd = random.Random(42)
+        if shuffle:
+            for group in groups:
+                rnd.shuffle(group)
+
+        # Count groups
+        groups_count = np.array([len(groups[key]) for key in groups.keys()])
+        for count, group in zip(groups_count, groups.keys()):
+            logging.warning(f'{group.upper()} count: {count}')
+        # Split files into train valid groups
+        valid_sizes = np.ceil(groups_count * valid_train_ratio).astype(int)
+        train_files = {key: groups[key][valid_size:] for key, valid_size in zip(groups.keys(), valid_sizes)}
+        valid_files = {key: groups[key][:valid_size] for key, valid_size in zip(groups.keys(), valid_sizes)}
+
+        train_files = _merge_items(train_files)
+        valid_files = _merge_items(valid_files)
+
+        if len(non_first_visit_files) > 0:
+            logging.warning(f'Filtering non first visit count:{len(non_first_visit_files)}, appending to allowed set')
+            if not train_filter_first_screen:
+                train_files = train_files + non_first_visit_files
+            if not valid_filter_first_screen:
+                valid_files = valid_files + non_first_visit_files
+
+        return train_files, valid_files
