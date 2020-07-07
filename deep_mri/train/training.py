a610a6393c8a66735f59963fb95b605d94794251
Tomas Karella <tomas.karella@profinit.eu>
2020-07-07 19:38:10 +0200

    Overriding config option

--- deep_mri/train/training.py
+++ deep_mri/train/training.py
@@ -1,12 +1,20 @@
 import json
-
+import logging
 from .config_parser import config_to_ds, config_to_model, config_to_callbacks, config_batch_size, config_epochs
 
 
-def run_train(path_to_config):
+def run_train(path_to_config, overriding_config):
     # load config
     with open(path_to_config, 'r') as f:
         config = json.load(f)
+
+    for key, value in overriding_config.items():
+        logging.warning(f"Overriding config {key} to {value}")
+        config[key] = value
+
+    # TODO: Copy config callback
+    # TODO: Other stats,
+    # TODO: Iterate over various stuff
 
     train_ds, valid_ds = config_to_ds(config)
     model = config_to_model(config)
